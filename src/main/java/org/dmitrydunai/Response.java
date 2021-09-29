9abd2e3cd941cb64b55cfe4ee756977a373fa954
egachok <egachok@gmail.com>
2021-09-29 20:48:44 +0300

    Initial commit

---
+++ src/main/java/org/dmitrydunai/Response.java
@@ -0,0 +1,12 @@
+package org.dmitrydunai;
+
+import lombok.Data;
+import java.util.Map;
+
+@Data
+public class Response{
+    private String statusText;
+    private int statusCode;
+    private Map<String,String> headers;
+    private String payload;
+}
