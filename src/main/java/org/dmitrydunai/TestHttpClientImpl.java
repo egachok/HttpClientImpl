62dc1c3e3191f6adc686a8b7c508bc98d3664bb1
egachok <egachok@gmail.com>
2021-09-29 22:18:45 +0300

    created main class TestHttpClientImpl and tested custom http client --> successfully.

--- src/main/java/org/dmitrydunai/TestHttpClientImpl.java
+++ src/main/java/org/dmitrydunai/TestHttpClientImpl.java
@@ -1,7 +1,17 @@
 package org.dmitrydunai;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TestHttpClientImpl {
     public static void main(String[] args) {
 
+        HttpClientImpl httpClient = new HttpClientImpl();
+        Response response;
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put("Connection", "text/html");
+
+        response = httpClient.get("http://www.google.com", headers);
+        System.out.println(response);
     }
 }
